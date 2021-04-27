aea56013bb5878ec200ea98b96dd1b8057ff9711
lalapolalaanewb <mohdfathi_mhdnoor@yahoo.com>
2021-04-27 19:05:44 +0800

    login screen part 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
-import HomeScreen from './HomeScreen'
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
+  const user = null
+
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="*" component={() => "404 Page Not Found!"} />
+          </Switch>
+        )}
+      </Router>
     </div>
   );
 }
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SignupScreen.css'
+
+function SignupScreen() {
+  return (
+    <div className="signupScreen">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  )
+}
+
+export default SignupScreen
