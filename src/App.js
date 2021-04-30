2ead72ab668ad6de66cd3bedf1d23ea6cd551721
lalapolalaanewb <mohdfathi_mhdnoor@yahoo.com>
2021-05-01 07:33:58 +0800

    redux implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 import HomeScreen from './screens/HomeScreen'
+import ProfileScreen from './screens/ProfileScreen'
 import LoginScreen from './screens/LoginScreen'
 
 function App() {
-  const user = null
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+
+    // clean up
+    return unsubscribe
+  }, [dispatch])
 
   return (
     <div className="app">
@@ -15,6 +36,7 @@ function App() {
         ) : (
           <Switch>
             <Route exact path="/" component={HomeScreen} />
+            <Route path="/profile" component={ProfileScreen} />
             <Route path="*" component={() => "404 Page Not Found!"} />
           </Switch>
         )}
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -6,3 +8,10 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export { auth }
+export default db
